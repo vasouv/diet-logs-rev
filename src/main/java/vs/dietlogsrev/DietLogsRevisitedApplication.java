3ff33aafef7c8da15f71c03ca77887e2a77d10ec
Vasilis Souvatzis <vasouv@gmail.com>
2021-06-15 15:48:48 +0300

    Initial commit

---
+++ src/main/java/vs/dietlogsrev/DietLogsRevisitedApplication.java
@@ -0,0 +1,13 @@
+package vs.dietlogsrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietLogsRevisitedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietLogsRevisitedApplication.class, args);
+	}
+
+}
